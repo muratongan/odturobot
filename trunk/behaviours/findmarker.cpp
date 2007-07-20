eb1888725d286109e60f791a93e9a34e162f9a70
makbulegulcin <makbulegulcin@c752a663-4f35-0410-82e1-b15e11f8ce98>
2007-07-20 08:08:49 +0000

    line detection fixed

--- trunk/behaviours/findmarker.cpp
+++ trunk/behaviours/findmarker.cpp
@@ -6,18 +6,25 @@ int main(void)
 {
     CamClient client(9034);
     int* circle_info;
+    int margin = 5;
+    int margin2 = 10;
+    
     while(true) {
         circle_info = client.getCircles();
         if(circle_info[0]==2) {
-            cout<<"standartmarker"<<flush;
-            cerr<<"standartmarker";
-            return 0;
-        }
-        else if(circle_info[0]==3) {
-            cout<<"endmarker"<<flush;
-            cerr<<"endmarker";
-            return 0;
-        }
+            if((circle_info[3]-circle_info[6])<=margin && (circle_info[3]-circle_info[6])>=-margin)
+               { 
+                    cout<<"standartmarker"<<flush;
+                    cerr<<"standartmarker";
+                    return 0;
+                }
+            else if((circle_info[3]-circle_info[6])<=margin2 && (circle_info[3]-circle_info[6])>=-margin2)
+                {
+                    cout<<"endmarker"<<flush;
+                    cerr<<"endmarker";
+                    return 0;
+                }
+         }
      } 
 	sleep(1);
     return 0;
